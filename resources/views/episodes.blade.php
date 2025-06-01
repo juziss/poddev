4346e9eee29374f7b79a561cd21b2c7c14b833c3
juziss <julianeta26@gmail.com>
2025-06-01 12:01:35 -0300

    refactor: route name update

---
+++ resources/views/episodes.blade.php
@@ -0,0 +1,8 @@
+
+@extends('layouts.main')
+
+@section('title', 'Espisódios')
+
+    <h1>Lista de Episódios</h1>
+
+@endsection
--- resources/views/podcasts.blade.php
+++
@@ -1,8 +0,0 @@
-
-@extends('layouts.main')
-
-@section('title', 'Podcasts')
-
-    <h1>Lista de Podcasts</h1>
-
-@endsection
